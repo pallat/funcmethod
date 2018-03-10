b7815387761746968fca881f45c7cd97bde2f457
pallat <yod.pallat@gmail.com>
2018-03-10 22:21:40 +0700

    test function transform to implement interface

--- implement.go
+++ implement.go
@@ -6,6 +6,10 @@ func (w work) Do() (v interface{}, err error) {
 	return w()
 }
 
+func dosomething() (v interface{}, err error) {
+	return "cool", nil
+}
+
 func transform(s string) Worker {
 	return work(func() (v interface{}, err error) {
 		return s, nil
--- implement_test.go
+++ implement_test.go
@@ -5,6 +5,29 @@ import (
 )
 
 func TestTransformFuncToImplementInterface(t *testing.T) {
+	r, err := Handler(work(dosomething))
+	if err != nil {
+		t.Error("it should not error like this", err)
+		return
+	}
+
+	if _, ok := r.(map[string]interface{}); !ok {
+		t.Error("the return type should be map[string]interface{}")
+		return
+	}
+
+	if v, ok := r.(map[string]interface{})["message"]; ok {
+		if v != "cool" {
+			t.Error("it should be ok but got", v)
+			return
+		}
+		return
+	}
+
+	t.Error("the result should contains message key")
+}
+
+func TestTransformStringToImplementInterface(t *testing.T) {
 	w := transform("ok")
 	r, err := Handler(w)
 
